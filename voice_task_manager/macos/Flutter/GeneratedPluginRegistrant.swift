8f3953f6d1c9c31f434de877f53afd36c39dd532
Brayan-chan <al071392@uacam.mx>
2024-12-08 16:49:05 -0600

    Registrar el plugin SharedPreferences en GeneratedPluginRegistrant.swift

--- voice_task_manager/macos/Flutter/GeneratedPluginRegistrant.swift
+++ voice_task_manager/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import flutter_tts
+import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
