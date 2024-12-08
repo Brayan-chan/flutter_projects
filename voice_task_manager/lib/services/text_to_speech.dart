a98f31cc172d40ff50e9f6e087a5c08b09a75b34
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-12-08 17:14:49 -0600

    Actualizar text_to_speech.dart

--- voice_task_manager/lib/services/text_to_speech.dart
+++ voice_task_manager/lib/services/text_to_speech.dart
@@ -4,7 +4,7 @@ class TextToSpeech {
   final FlutterTts _tts = FlutterTts();
 
   Future<void> speak(String text) async {
-    await _tts.setLanguage("es-ES"); // Cambia a tu idioma preferido
+    await _tts.setLanguage("es-ES"); 
     await _tts.setPitch(1.0);
     await _tts.setSpeechRate(0.9);
     await _tts.speak(text);
